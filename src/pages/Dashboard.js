873f3f2741b649ed72b19ee6a127f8d5f181337e
meru20 <merhawitgubsa@gmail.com>
2021-03-29 10:23:47 -0700

    refactor(HeroCard.js):refactored the hero card component form dashboard.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,6 @@
 import {useState, useEffect} from 'react';
 import {heroData} from '../data/heroes';
-import {Link} from 'react-router-dom';
-import {FaStar, FaRegStar} from'react-icons/fa';
-import {GiBatMask} from'react-icons/gi';
+import HeroCardComponent from '../pages/HeroCard';
 
 const DashboardPage = () => {
     const [heroes, setHeroes] = useState(heroData);
@@ -20,44 +18,7 @@ const DashboardPage = () => {
             <h4 className='text-secondary'>Featured Hereos</h4>
             </div>
         </div>
-        <div className='row'>
-            {heroes.map ((hero,index) => {
-                return (
-                    <div className='col-sm-12 col-md-3' key={hero.id}>
-                        <div className='card mb-3'>
-                            <div className='card-header text-center'>
-                                {hero.publisher}
-                                </div>
-                                <img src={hero.image_url} alt={hero.superhero} className='card-img-top'/> 
-                                <div className='card-body'>
-                                    <h4 className='card-title'>{hero.superhero ==='Batman'? <GiBatMask/> : ''}{hero.superhero}</h4>
-                                    <h6 className='text-secondary' >"{hero.alter_ego}"</h6>
-                                    <div className='my-2'>
-                                        <strong>First Appearance:</strong>{hero.first_appearance}</div>
-                                        <div className='my-2'>
-                                            <strong>Characters:</strong>
-                                            {hero.characters.map((character,i) => {
-                                                return(
-                                                    <small>{character} {i === hero.characters.length -1 ? '':', '} </small>
-                                                )
-                                            })}
-                                        </div>
-                                       <div className='my-2 d-flex justify-content-between'>
-                                           <Link to={`/heroes/${hero.id}`} className='card-link'>View Details</Link>
-                                           <a href='' className='card-link'>
-                                               {!hero.featured ? <FaStar/>: <FaRegStar/>}
-                                               
-                                           </a>
-                                       </div> 
-                                </div>
-
-                        </div>  
-                    </div>          
-                )
-
-            })}
-
-        </div>
+       <HeroCardComponent heroes={heroes}/>
         </div>
     )
 }
